f1e0a89516ee5a7cac61c39452d787ce445e92e6
Vintohlet <ruslantuleuoff@gmail.com>
2025-04-13 22:58:43 +0500

    minor fix

--- models/User.js
+++ models/User.js
@@ -1,1 +1,37 @@
-c
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        required:true,
+        min: 0
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required:true
+    },
+    appointments:{
+          type: [mongoose.Schema.Types.ObjectId],
+          ref: "Appointment"
+    },
+    isManager:{
+        type: Boolean
+    }
+})
+
+export const User = mongoose.model("User", userSchema)
