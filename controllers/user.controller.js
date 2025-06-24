6fc48f33ad87ef8e3911ce8da885cc7655c03f8d
Vintohlet <ruslantuleuoff@gmail.com>
2025-06-24 15:04:23 +0500

    log of user id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -68,6 +68,7 @@ try {
 }
 async getMe(req, res) {
   try {
+    console.log("req.userId =>", req.userId);
     const user = await User.findById(req.userId).select("-password");
     if (!user) return res.status(404).json({ message: "User not found" });
 
